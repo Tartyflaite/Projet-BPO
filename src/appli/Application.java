b4fa7f754884c45448cf6be429aee308ef768e4d
Tartyflaite <71936440+Tartyflaite@users.noreply.github.com>
2021-02-15 22:19:26 +0100

    debut du proj
    on peu piocher des cartes et en poser, le main est pas fait et les tests unitaires non plus

--- src/appli/Application.java
+++ src/appli/Application.java
@@ -1,6 +1,6 @@
 package appli;
 
-public class Appli {
+public class Application {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ src/game/Joueur.java
@@ -0,0 +1,96 @@
+package game;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Joueur {
+	private ArrayList<Integer> deck;
+	private ArrayList<Integer> hand;
+	private int pileAsc; // pile ascendante
+	private int pileDsc; // pile descendante
+	private String nom;
+	
+	
+	
+	public String toString() {
+		StringBuilder sb= new StringBuilder();
+		sb.append(this.nom+" ");
+		sb.append("^["+pileAsc+"] ");
+		sb.append("v["+pileDsc+"] ");
+		sb.append("(m"+this.hand.size());
+		sb.append("p"+this.deck.size()+")");
+		return sb.toString();
+	}
+	
+	public Joueur(String nom) {
+		this.deck= new ArrayList<>();
+		for(int i=2;i<60;++i) {
+			this.deck.add(i);
+		}
+		this.hand= new ArrayList<>();
+		this.pileAsc=1;
+		this.pileDsc=60;
+		this.nom=nom;
+	}
+	
+	public String piocher(int nb_cartes) {
+		int i;
+		Random pioche = new Random();
+		for(i=0; i<nb_cartes && this.deck.size()>0;++i) {
+			int carte= deck.get(pioche.nextInt(this.deck.size()));
+			hand.add(carte);
+			deck.remove(carte);
+		}
+		return i +" cartes piochées";
+
+	}
+	
+	public boolean isInHand(int carte) {
+		return hand.contains(carte);
+	}
+	
+	public boolean estJouable(Joueur j,int carte) {
+		
+		return this.estJouableAsc(carte) || this.estJouableDsc(carte) || this.estJouableAscAdv(j, carte) || this.estJouableDscAdv(j, carte);
+	}
+	
+	public boolean estJouableAsc(int carte) {
+		return (carte>pileAsc) || (carte==pileAsc-10);
+	}
+	
+	public boolean estJouableDsc(int carte) {
+		return (carte<pileDsc) || (carte==pileDsc+10);
+	}
+	
+	public boolean estJouableAscAdv(Joueur j,int carte) {
+		return (carte<j.pileAsc);
+	}
+	
+	public boolean estJouableDscAdv(Joueur j,int carte) {
+		return (carte>j.pileDsc);
+	}
+	
+	public void placerAsc(Joueur j, int carte) {
+		if(this.equals(j)) {
+			this.pileAsc=carte;
+			this.hand.remove(carte);
+		}
+		else {
+			j.pileAsc=carte;
+			this.hand.remove(carte);
+		}
+	}
+	
+	public void placerDsc(Joueur j, int carte) {
+		if(this.equals(j)) {
+			this.pileDsc=carte;
+			this.hand.remove(carte);
+		}
+		else {
+			j.pileDsc=carte;
+			this.hand.remove(carte);
+		}
+	}
+	
+	
+}
