47df4a3db43fc70ed3cda6fd3b858c6a202e59ec
Tartyflaite <71936440+Tartyflaite@users.noreply.github.com>
2021-02-21 16:45:26 +0100

    youyou

--- src/appli/Application.java
+++ src/appli/Application.java
@@ -1,10 +1,37 @@
 package appli;
 
+import java.util.Scanner;
+
+import game.Joueur;
+
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Joueur nord= new Joueur("NORD");
+		Joueur sud= new Joueur("SUD");
+		
+		
+//			for (int i = 1; i < 20; ++i)
+//			System.out.println(String.format("%02d", i));
+		
+		@SuppressWarnings("resource")
+		Scanner sc = new Scanner(System.in);
+		String s;
+		System.out.print("> ");
+		s = sc.nextLine();
+		while (!s.equals("fin")) {
+			décompose(s);
+			System.out.print("> ");
+			s = sc.nextLine();
+		}
 	}
-
+	
+	
+	private static void décompose(String s) {
+		String[] tab = s.split("\\s+");
+		for (String mot : tab)
+			System.out.println(mot);
+	}
+	
+	
 }
