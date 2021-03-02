eec064f249e5872ae11225241dd75e26f8dcc47c
likir09 <luca.randazzo@protonmail.com>
2021-03-02 11:20:12 +0100

    Pioche & main
    Implémentation du mélange de la pioche et de la récupération des 6 premières cartes

---
+++ Application.java
@@ -0,0 +1,17 @@
+package appli;
+
+import java.util.ArrayList;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        Pile p = new Pile("NORD");
+        Pile p2 = new Pile("SUD ");
+
+        p.afficher();
+
+
+    }
+
+}
