b5c9403dd63d1faafa9d3b29ddaddcf03878b666
likir09 <luca.randazzo@protonmail.com>
2021-03-02 15:30:23 +0100

    Update Application.java

--- src/appli/Application.java
+++ src/appli/Application.java
@@ -8,12 +8,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		Joueur nord= new Joueur("NORD");
-		Joueur sud= new Joueur("SUD");
-		
-		
-//			for (int i = 1; i < 20; ++i)
-//			System.out.println(String.format("%02d", i));
-		
+		Joueur sud= new Joueur("SUD ");
+
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		String s;
@@ -25,16 +21,16 @@ public class Application {
 			s = sc.nextLine();
 		}
 	}
-	
-	
+
+
 	private static void traitement(String s) {
 		String[] tab = s.split("\\s+");
 		for (String mot : tab) {
 			if (mot.length()==3 || mot.length()==4) {
-				
+
 			}
 		}
-			
+
 	}
-	
+
 }
